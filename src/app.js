938cff513a9b80d7d2db3d79d2e059961536367d
Elijah Simpson <elijahsimpson1991@gmail.com>
2021-03-07 16:19:53 -0500

    created app.js file

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const { NODE_ENV } = require("./config");
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+
+const app = express();
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+//TODO
+//Set routes for App
+
+app.use(function errorHandler(error, req, res) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
